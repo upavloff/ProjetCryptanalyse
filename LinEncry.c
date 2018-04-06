26dee8152cb8fc4ae5cd95c9a8d50f03f3132681
Roukira <yyy.yousfi@gmail.com>
2018-04-06 18:19:11 +0200

    LinEncry Debug NE PAS UTILISER
    NE PAS UTILISER

--- LinEncry.c
+++ LinEncry.c
@@ -14,15 +14,13 @@ void createZi(int8_t *Z, int8_t *L, int8_t *K, int8_t *F, unsigned int n){
 	 int8_t *KLi = allocateVector(n);
 	 int8_t zi;
 	 copyVector(KLi,K,n);
-	 for(i = 0; i < n ; ++i){
+	 for(i = 0; i < n ; i++){
 		zi = 0;	
 		vectorMatrixProduct(KLiTemp,KLi,L,n,n);
-		printf("Voici la matrice KLitemp :\n");
-		printMatrix(KLiTemp,n,n);
 		copyVector(KLi,KLiTemp,n);
-		printf("Voici la matrice KLi :\n");
-		printMatrix(KLi,n,n);
-		for(j = 0; j < n; ++j){
+		//printf("Voici le vecteur KLi :\n");
+		//printVector(KLi,n);
+		for(j = 0; j < n; j++){
    			zi ^= KLi[j] & F[j];
   		}
   		Z[i] = zi;
@@ -30,18 +28,20 @@ void createZi(int8_t *Z, int8_t *L, int8_t *K, int8_t *F, unsigned int n){
 	}
 	free(KLiTemp);
 	free(KLi);
-	printVector(Z,n);
+	//printVector(Z,n);
 }
 
 void createSystem(int8_t* Sys, int8_t *F, int8_t* L, unsigned int n){
 	unsigned int i, j, k;
 	int8_t* Li = allocateMatrix(n,n);
 	int8_t* L2 = allocateMatrix(n,n);
-	printf("L : \n");
-	printMatrix(L,n,n);
+	//printf("L : \n");
+	//printMatrix(L,n,n);
 	identityMatrix(L2, n, n);
 	for(i = 0; i < n; ++i){
 		matrixMatrixProduct(Li, L2, L, n, n, n);
+		printf("Voici L^%u : \n",i+1);
+		//printMatrix(Li,n,n);
 		for(j = 0; j < n; ++j){
 			Sys[LIN(i,j,n,n)] = 0;
 			for(k = 0; k < n; ++k){
@@ -52,21 +52,22 @@ void createSystem(int8_t* Sys, int8_t *F, int8_t* L, unsigned int n){
 		
 	
 	}
-	printf("debug1\n");
+	//printf("debug1\n");
 	freeMatrix(L2);
-	printf("debug2\n");
+	//printf("debug2\n");
 
 	freeMatrix(Li);
 
 }
-void decrypt(int8_t *res, int8_t *L, int8_t *Z, int8_t *F, unsigned int n){
+int decrypt(int8_t *res, int8_t *L, int8_t *Z, int8_t *F, unsigned int n){
 	int8_t* Sys = allocateMatrix(n,n);	
 	createSystem(Sys, F, L, n);
 	printf("Voici le systeme lineaire : \n");
 	printMatrix(Sys,n,n);
 	printf("Qui foit un vector X doit valoir : \n");
 	printVector(Z,n);
-	solveSystemGauss(res, Sys, Z, n);
+	int boolean = solveSystemGauss(res, Sys, Z, n);
 	freeMatrix(Sys);
+	return boolean;
 }
 
--- LinEncry.h
+++ LinEncry.h
@@ -5,7 +5,7 @@ void createZi(int8_t *Z, int8_t *L, int8_t *K, int8_t *F, unsigned int n);
 
 void createSystem(int8_t* Sys, int8_t *F, int8_t* L, unsigned int n);
 
-void decrypt(int8_t *res, int8_t *L, int8_t *Z, int8_t *F, unsigned int n);
+int decrypt(int8_t *res, int8_t *L, int8_t *Z, int8_t *F, unsigned int n);
 
 
 #endif
