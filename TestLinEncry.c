268f3b984dadb6e6e4b097dea0995d98ab5b76a5
Roukira <yyy.yousfi@gmail.com>
2018-04-06 18:41:03 +0200

    LinEncry fonctionnel

--- TestLinEncry.c
+++ TestLinEncry.c
@@ -9,51 +9,54 @@
 int main(int argc, char ** argv){
 	unsigned int n = 20;
 	if(argc == 2) n = (unsigned int)atoi(argv[1]);
-	printf("debug\n");
 	int8_t *K = allocateVector(n);
-	printf("debug\n");
 	int8_t *L = allocateMatrix(n,n);
-	printf("debug\n");
 	int8_t *F = allocateVector(n);
-	printf("debug\n");
 	int8_t *Z = allocateVector(n);
-	printf("debug\n");
-	
+	int8_t *Kcalc = allocateVector(n);
 	
 	srand(time(NULL));
+	do{
+		randomBinVector(K,n);
+		printf("Voici K : \n");
+		printVector(K,n);
 	
-	randomBinVector(K,n);
-	printf("Voici K : \n");
-	printVector(K,n);
+		randomBinVector(F,n);
+		printf("Voici F : \n");
+		printVector(F,n);
 	
-	randomBinVector(F,n);
-	printf("Voici F : \n");
-	printVector(F,n);
-	
-	randomBinMatrix(L,n,n);
-	printf("Voici L : \n");
-	printMatrix(L,n,n);
-	
-	
-	
-//==========CREATION Zi===============	
-	
-	createZi(Z,L,K,F,n);
-	
-	
-	printf("Voici Z : \n");
-	printVector(Z,n);
-	
-	printf("debug\n");
-	int8_t *Kcalc = allocateVector(n);
-	printf("debug\n");
-	decrypt(Kcalc,L,Z,F,n);
+		randomBinMatrix(L,n,n);
+		printf("Voici L : \n");
+		printMatrix(L,n,n);
+		
+		createZi(Z,L,K,F,n);
+		printf("Voici Z : \n");
+		printVector(Z,n);
+		
+	}while(!decrypt(Kcalc,L,Z,F,n));
 	
 	printf("Rappel de K : \n");
 	printVector(K,n);
 	
 	printf("Voici K2 : \n");
 	printVector(Kcalc,n);
+	
+	
+	
+	printf("Voici Z : \n");
+	printVector(Z,n);
+	createZi(Z,L,Kcalc,F,n);
+	
+	printf("Voici le nouveau Z calcule a partir de K2: \n");
+	printVector(Z,n);
+	
+	int k = 0,sum = 0;
+	for(k=0;k<n;k++){
+		if (K[k]==Kcalc[k]) sum++;
+	}
+	printf("Comparaison entre K et K2 : \n");
+	printf("nombres qui coincident par rapport a %d : %d\n",n,sum);
+	
 	
 	free(K);
 	free(L);
